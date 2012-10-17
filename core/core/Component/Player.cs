691716c0a6a9262dad517e1541e532b7233da99e
topolyai5 <gerely01@gmail.com>
2012-10-18 00:15:24 +0200

    refactoring

---
+++ core/core/Component/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using core.Service;
+
+namespace core.Component
+{
+    public class Player : DrawableGameComponent
+    {
+        private PlayerService playerService;
+
+        public override void Update(GameTime time)
+        {
+            playerService.receiveDamage(10f);
+            base.Update(time);
+        }
+    }
+}
---
+++ core/core/Domain/Statistics.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace core.Domain
+{
+    public class Statistics
+    {
+        private float healtPoint;
+        private float maxHealtPoint;
+        private bool isDead;
+
+        public float HealthPoint
+        {
+            set
+            {
+                healtPoint = value;
+            }
+            get
+            {
+                return healtPoint;
+            }
+        }
+
+        public float MaxHealthPoint
+        {
+            set
+            {
+                maxHealtPoint = value;
+            }
+            get
+            {
+                return maxHealtPoint;
+            }
+        }
+
+        public bool IsDead
+        {
+            set
+            {
+                isDead = value;
+            }
+            get
+            {
+                return isDead;
+            }
+        }
+    }
+}
