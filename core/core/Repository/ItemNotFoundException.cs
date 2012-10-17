68f2ee00e922ff6b1bc4674924033f6d59dad4b7
topolyai5 <gerely01@gmail.com>
2012-10-17 23:50:12 +0200

    added repositories and its implementations

---
+++ core/core/Repository/ItemNotFoundException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace core.Repository
+{
+    public class ItemNotFoundException  :Exception
+    {
+        public ItemNotFoundException()
+            : base("Item not found!")
+        {
+
+        }
+
+        public ItemNotFoundException(string msg) 
+            : base(msg)
+        {
+
+        }
+    }
+}
---
+++ core/core/Repository/ItemRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using core.Domain;
+
+namespace core.Repository
+{
+    public interface ItemRepository
+    {
+        List<Item> getAllItems();
+        Item getItem(ItemName itemName);
+        void addItem(Item item);
+        void removeItem(Item item);
+    }
+}
