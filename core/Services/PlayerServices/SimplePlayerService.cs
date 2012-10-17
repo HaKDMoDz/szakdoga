dae4ffb57143e58d809716a7eb75c8a68b992fb1
topolyai5 <gerely01@gmail.com>
2012-10-17 19:45:00 +0200

    added Services project

---
+++ core/Services/PlayerServices/SimplePlayerService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using core.Service;
+using core.Domain;
+
+namespace Services.PlayerServices
+{
+    public class SimplePlayerService : PlayerService
+    {
+        private Player player;
+
+        public Player Player
+        {
+            set
+            {
+                this.player = value;
+            }
+        }
+
+        public void rebirth()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void kill()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void attack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void receiveDamage(float damage)
+        {
+            player.HealthPoint -= damage;
+            if (player.HealthPoint <= 0)
+            {
+                playerDead();
+            }
+        }
+
+        private void playerDead()
+        {
+            player.IsDead = true;
+        }
+    }
+}
