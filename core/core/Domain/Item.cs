49d255661108a5651db296bef93d995b5a8d5cb3
topolyai5 <gerely01@gmail.com>
2012-10-17 20:19:22 +0200

    added Item domain

---
+++ core/core/Domain/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace core.Domain
+{
+    public class Item
+    {
+        public String Name { get; set; }
+        public ItemType ItemType { get; set; }
+        public ItemClass ItemClass { get; set; }
+        public float MagicDefense { get; set; }
+    }
+}
