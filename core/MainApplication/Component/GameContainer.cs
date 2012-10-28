433805483a5e38f38845b9bccd8dc66dc3522746
topolyai5 <gerely01@gmail.com>
2012-10-28 14:48:27 +0100

    refactoring GameContainer

--- core/MainApplication/Component/GameContainer.cs
+++ core/MainApplication/Component/GameContainer.cs
@@ -6,13 +6,13 @@ using core.Component;
 using core;
 using MainApplication.Component.Land;
 using MainApplication.Component.Characters;
-using MainApplication.Component.Cameras;
 using core.Domain;
 using core.Service;
 using Services.PlayerServices;
 using core.Repository;
-using Repositories.MemoryBaseRepository;
 using Services.ItemServices;
+using Repositories.ItemRepositories;
+using Services.CameraServices;
 
 namespace MainApplication.Component
 {
@@ -21,12 +21,19 @@ namespace MainApplication.Component
         private Game game;
 
         private Player player;
+        private Statistics statistics;
+        private CameraStat cameraStat;
 
         private Dictionary<String, Object> objects = new Dictionary<string, object>();
 
         private PlayerService playerService;
         private ItemService itemService;
+        private CameraService cameraService;
         private ItemRepository itemRepository;
+        private InputManager inputManager;
+
+
+        private Camera camera;
 
         public GameContainer(Game game)
         {
@@ -34,34 +41,72 @@ namespace MainApplication.Component
         }
         public void start()
         {
-            //addComponent(new Player(game));
-            //addComponent(new Rancor(game));
-            //addComponent(new SunLigth(game));
-            //addComponent(new FollowCamera(game));
-
+            initComponent();
+            initDomain();            
             initServices();
+            injectDependency();
+            loadComponents();
+        }
+
+        private void loadComponents()
+        {
+            foreach (var item in game.Components)
+            {
+                item.Load();
+            }
+        }
+
+        private void initDomain()
+        {
+            cameraStat = new CameraStat();
+            cameraStat.Distance = 20;
+            cameraStat.AngleX = 180;
+            cameraStat.AngleY = -30;
+
+            statistics = new Statistics();
+            statistics.HealthPoint = 100;
+            statistics.IsDead = false;
+            statistics.MaxHealthPoint = 100;
+        }
+
+        private void injectDependency()
+        {
+            camera.CameraStat = cameraStat;
+            camera.CameraService = cameraService;
+        }
+
+        private void initComponent()
+        {
+            camera = new Camera(game);
+            addComponent(camera);
+            addComponent(new Rancor(game));
+            addComponent(new SunLigth(game));
+            inputManager = new InputManager(game);
+            addComponent(inputManager);
         }
 
         private void addComponent(GameComponent component)
         {
             game.Components.Add(component);
-            component.Load();
         }
 
         private void initServices()
         {
             playerService = new SimplePlayerService();
-            ((SimplePlayerService)playerService).Statistics = player;
+            ((SimplePlayerService)playerService).Statistics = statistics;
 
             itemRepository = new MemoryItemRepository();
 
             itemService = new SimpleItemService();
             ((SimpleItemService)itemService).ItemRepository = itemRepository;
 
+            cameraService = new FollowCameraService(cameraStat, inputManager);
+
 
             registerObject("playerService", playerService);
             registerObject("itemRepository", itemRepository);
             registerObject("itemService", itemService);
+            registerObject("cameraService", cameraService);
             
         }
 
