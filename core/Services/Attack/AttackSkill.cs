324a1e87e818b586550acd0ea3adaf7e96ae859a
topolyai5 <gerely01@gmail.com>
2012-10-28 17:39:03 +0100

    added new skillStrategy implementation

---
+++ core/Services/Attack/AttackSkill.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using core.Service.attack;
+using core.Domain;
+
+namespace Services.Attack
+{
+    public class AttackSkill : SkillStrategy
+    {
+        public void use(Character target, Skill skill)
+        {
+            
+
+            target.damage(100);
+        }
+    }
+}
