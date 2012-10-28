06d709dfdd8f4c858dc72e1a2827848eaa8592e2
topolyai5 <gerely01@gmail.com>
2012-10-28 17:39:30 +0100

    added new domain

---
+++ core/core/Domain/Character.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace core.Domain
+{
+    public interface Character
+    {
+        void damage(float damage);
+        void addBuff();
+        void heal(float healtPoint);
+        void curse();
+    }
+}
