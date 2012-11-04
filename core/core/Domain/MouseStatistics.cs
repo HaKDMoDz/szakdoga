3372396227716bbd41781bc5940a20e2a3717be4
topolyai5 <gerely01@gmail.com>
2012-11-04 20:14:35 +0100

    added MouseStat domain
    The mouse statistics is stored

---
+++ core/core/Domain/MouseStatistics.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace core.Domain
+{
+    public class MouseStatistics
+    {
+        private bool rightMouseButton, leftMouseButton, dummy, middleMouseButton;
+        private int mouseX, mouseY, mouseScroll;
+        private int mAbsPosX, mAbsPosY;
+
+        public bool MiddleMouseButton
+        {
+            get
+            {
+                return middleMouseButton;
+            }
+        }
+
+        public bool RightMouseButton
+        {
+            get
+            {
+                return rightMouseButton;
+            }
+        }
+
+        public bool LeftMouseButton
+        {
+            get
+            {
+                return leftMouseButton;
+            }
+        }
+
+        public float MouseX
+        {
+            get
+            {
+                return mouseX;
+            }
+        }
+
+        public float MouseY
+        {
+            get
+            {
+                return mouseY;
+            }
+        }
+
+        public float Scroll
+        {
+            get
+            {
+                return mouseScroll;
+            }
+        }
+    }
+}
