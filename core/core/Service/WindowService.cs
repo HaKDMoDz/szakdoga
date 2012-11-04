768313165a48ed15c0c57b8a4a2634954cb30519
topolyai5 <gerely01@gmail.com>
2012-11-04 22:56:28 +0100

    updated WindowService interface

--- core/core/Service/WindowService.cs
+++ core/core/Service/WindowService.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using core.Windows;
+using core.Domain;
 
 namespace core.Service
 {
     public interface WindowService
     {
+        void addWindow(WindowsName windowName, WindowsBase window);
+        void removeWindow(WindowsName windowName);
+        WindowsBase getWindow(WindowsName windowName);
+        void showWindow(WindowsName windowName);
+        void closeWindow(WindowsName windowName);
+        bool clickInWindowArea();
     }
 }
