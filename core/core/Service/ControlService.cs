42920a4ba05b58f15cd27f64805a098fcd97104f
topolyai5 <gerely01@gmail.com>
2012-11-04 20:19:37 +0100

    added new method for ControlService

--- core/core/Service/ControlService.cs
+++ core/core/Service/ControlService.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using core.Collision;
+using MTV3D65;
 
 namespace core.Service
 {
     public interface ControlService
     {
-
+        void goToTarget(TV_3DVECTOR targetPos);
+        void turnToTargetDirection(DetectCollision collision);
     }
 }
