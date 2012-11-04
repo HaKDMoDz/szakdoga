c8ad29e8cef6bba3da3edbe8c4e2167351dfa95c
topolyai5 <gerely01@gmail.com>
2012-11-04 20:15:40 +0100

    added DetectCollision implementations

---
+++ core/core/Collision/Actor/ActorDetectCollision.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MTV3D65;
+
+namespace core.Collision.Actor
+{
+    public class ActorDetectCollision : DetectCollision
+    {
+        private TV_COLLISIONRESULT collResult;
+
+        public ActorDetectCollision(TV_COLLISIONRESULT collResult)
+        {
+            this.collResult = collResult;
+        }
+
+        public TV_3DVECTOR getTargetPosition()
+        {
+            return collResult.vCollisionImpact;
+        }
+    }
+}
---
+++ core/core/Collision/Landscape/LandscapeDetectCollision.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MTV3D65;
+
+namespace core.Collision.Landscape
+{
+    public class LandscapeDetectCollision : DetectCollision
+    {
+        private TV_COLLISIONRESULT collResult;
+
+        public LandscapeDetectCollision(TV_COLLISIONRESULT collResult)
+        {
+            this.collResult = collResult;
+        }
+
+        public TV_3DVECTOR getTargetPosition()
+        {
+            return collResult.vCollisionImpact;
+        }
+    }
+}
