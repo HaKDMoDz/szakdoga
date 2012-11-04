f365f07afb5a1a59e3040646d861b9c4317e9166
topolyai5 <gerely01@gmail.com>
2012-11-04 20:16:01 +0100

    added DetectCollision interface

---
+++ core/core/Collision/DetectCollision.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MTV3D65;
+
+namespace core.Collision
+{
+    public interface DetectCollision
+    {
+        TV_3DVECTOR getTargetPosition();
+    }
+}
